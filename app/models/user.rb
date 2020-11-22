0e93350f4911653be9f103e4d2210a211e47ec1a
MASAKI492 <66226599+MASAKI492@users.noreply.github.com>
2020-11-22 20:41:35 +0900

    ユーザー管理実装修正＃１

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
 
-  validates :nickname, presence: true
   with_options presence: true do
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name,      format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :last_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :password,        format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i }
-  end
-  validates :birth_date, presence: true
+    validates :nickname
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+    validates :last_name
+    validates :first_name
+    end
 
-  # VALID_PASSWORD_REGIX = /\A[a-z0-9]+\z/i
-  # validates :password, format: { with: VALID_PASSWORD_REGIX }
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+    end
+
+    validates :password,        format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i }
+    validates :birth_date
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,29 +9,6 @@ RSpec.describe User, type: :model do
       it 'nickname, password, password_confirmation, email, last_name, first_name, last_name_kana, first_name_kana, birth_dateがあれば登録できる' do
         expect(@user).to be_valid
       end
-      it 'passwordが6文字以上であれば登録できる' do
-        @user.password = 'aaa111'
-        @user.password_confirmation = 'aaa111'
-        expect(@user).to be_valid
-      end
-      it 'passwordが半角英数字があれば登録できる' do
-        @user.password = 'aaA111'
-        @user.password_confirmation = 'aaA111'
-        expect(@user).to be_valid
-      end
-      it 'ユーザー本名が全角（漢字、ひらがな、カタカナ）であれば登録できる' do
-        @user.last_name = '森,もり,モリ'
-        @user.first_name = '森,もり,モリ'
-        expect(@user).to be_valid
-      end
-      it 'ユーザー本名のフリガナが全角（カタカナ）であれば登録できる' do
-        @user.last_name_kana = 'モリ'
-        @user.first_name_kana = 'モリ'
-        expect(@user).to be_valid
-      end
-      it 'birth_dateがあれば登録できる' do
-        @user.birth_date = '0000-00-00'
-      end
     end
 
     context '新規登録がうまくいかない時' do
@@ -45,20 +22,41 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
+      it 'emailが登録済みの場合登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+      it 'emailに@が含まれていなければ登録できないこと' do
+        @user.email = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
       it 'passwordが空だと登録できない' do
         @user.password = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
-      it 'passwordが半角英数字でなければ登録できない' do
+      it 'passwordが半角英字のみの場合登録できない' do
         @user.password = 'aaaaaa'
-        # binding.pry
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'passwordが半角数字のみの場合登録できない' do
+        @user.password = '111111'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'passwordが全角英数字のみの場合登録できない' do
+        @user.password = '１１１AAA'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
       it 'passwordが5文字以下だと登録できない' do
-        @user.password = '00000'
-        @user.password_confirmation = '00000'
+        @user.password = '000aa'
+        @user.password_confirmation = '000aa'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
@@ -73,9 +71,9 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include('Last name is invalid')
       end
       it 'ユーザー本名が全角（漢字、ひらがな、カタカナ）でなければ登録できない' do
-        @user.first_name = 'a'
+        @user.first_name = 'b'
         @user.valid?
-        expect(@user.errors.full_messages).to include('First name is invalid')
+        expect(@user.errors.full_messages).to include("First name is invalid")
       end
       it 'ユーザー本名のフリガナが全角（カタカナ）でなければ登録できない' do
         @user.last_name_kana = '森,もり,mori'
