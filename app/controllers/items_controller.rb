749e3a088157201f7b1ae2654135af52eebd96f6
MASAKI492 <66226599+MASAKI492@users.noreply.github.com>
2020-11-22 20:53:46 +0900

    ユーザー管理機能修正＃２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,16 +3,5 @@ class ItemsController < ApplicationController
     @user = User.all
   end
 
-  # def new
-  #   @User = User.new
-  # end
-
-  # def create
-  # end
-
-  # private
-
-  # def user.params
-  #   params.require(:user).permit(:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date)
-  # end
+ 
 end
